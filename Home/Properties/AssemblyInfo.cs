ca1b0e1cad45b6be960ca35325eb1ab712b2f3f4
TeixeiraBriel <teixeirabriel@gmail.com>
2021-03-05 19:56:52 -0300

    v1.2.1.2103 -- NoResize

--- Home/Properties/AssemblyInfo.cs
+++ Home/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("Home")]
+[assembly: AssemblyTitle("Brielina Utilitarios")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Home")]
+[assembly: AssemblyProduct("Brielina Utilitarios")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.1.2103")]
+[assembly: AssemblyFileVersion("1.2.1.2103")]
