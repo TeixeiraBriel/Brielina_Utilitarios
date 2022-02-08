14677e3121e19be7dcf970503f11c8f35c8363f6
Gabriel Teixeira <biel.gt@gmail.com>
2022-02-08 12:54:33 -0300

    v2.3.2.2202

--- Home/Properties/AssemblyInfo.cs
+++ Home/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.1.2111")]
-[assembly: AssemblyFileVersion("2.3.1.2111")]
+[assembly: AssemblyVersion("2.3.2.2202")]
+[assembly: AssemblyFileVersion("2.3.2.2202")]
