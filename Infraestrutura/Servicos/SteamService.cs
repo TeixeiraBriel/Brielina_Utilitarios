4b07c4ae1a17eadbac5f7feb0e0ae067efadac75
Gabriel Teixeira <Gabriel Teixeira@GABRIELTPC>
2024-01-25 12:30:09 -0300

    Removendo redundancias

--- Infraestrutura/Servicos/SteamService.cs
+++ Infraestrutura/Servicos/SteamService.cs
@@ -24,7 +24,7 @@ namespace Infraestrutura.Servicos
         public async Task<OwnedGames> minerarPerfil(string idSteam)
         {
             var client = new RestClient($"https://api.steampowered.com/");
-            var request = new RestRequest("ISteamUser/GetPlayerSummaries/v0002/?key=94FADF560260531F6AEDF05CD43EEC9B&steamids={idSteam}", Method.Get);
+            var request = new RestRequest($"ISteamUser/GetPlayerSummaries/v0002/?key=94FADF560260531F6AEDF05CD43EEC9B&steamids={idSteam}", Method.Get);
             var response = await client.ExecuteAsync(request);
 
             OwnedGames resposta = JsonConvert.DeserializeObject<OwnedGames>(response.Content);
@@ -35,7 +35,7 @@ namespace Infraestrutura.Servicos
         {
 
             var client = new RestClient($"http://api.steampowered.com/");
-            var request = new RestRequest("IPlayerService/GetOwnedGames/v0001/?key=94FADF560260531F6AEDF05CD43EEC9B&steamid={idSteam}&include_appinfo=true&include_played_free_games={jogosGratis}&format=json",
+            var request = new RestRequest($"IPlayerService/GetOwnedGames/v0001/?key=94FADF560260531F6AEDF05CD43EEC9B&steamid={idSteam}&include_appinfo=true&include_played_free_games={jogosGratis}&format=json",
                 Method.Get);
             var response = await client.ExecuteAsync(request);
 
